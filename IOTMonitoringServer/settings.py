f6b5d2e5367a01658cea65b3dfce1109fcb01f8f
jp-hidalgo <36314124+jp-hidalgo@users.noreply.github.com>
2023-09-25 14:17:44 -0500

    Update settings.py

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "44.202.135.12"]
+ALLOWED_HOSTS = ["localhost", " 100.26.148.68"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "44.205.248.159",  # Dirección IP de la base de datos
+        "HOST": "3.227.21.124",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "100.24.3.58"
+MQTT_HOST = "44.200.196.157"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
